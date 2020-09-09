78991cae095bbdd04b71ab5fa866455f5f840670
Samriddhi <69254066+sam-810@users.noreply.github.com>
2020-09-09 18:30:19 +0530

    Update app.js

--- webscraper/app.js
+++ webscraper/app.js
@@ -2,24 +2,23 @@ var express = require("express");
 
 var PORT = process.env.PORT || 3000;
 
-// Initialize Express
+
 var app = express();
 
-// Configure middleware
-// Parse request body as JSON
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Make public a static folder
+
 app.use(express.static("public"));
 
-// Simple index route
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname + "./public/index.html"));
   });
   
 
-// Start the server
+
 app.listen(PORT, function() {
   console.log("App listening on port " + PORT);
 });
@@ -27,7 +26,7 @@ app.listen(PORT, function() {
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-/* Test Axios with Cheerio */
+
 axios.get("https://idioms.thefreedictionary.com/light").then(function(response) {
    
     var $ = cheerio.load(response.data);
@@ -57,11 +56,11 @@ scrape(req.params.searchTerm)
     foundIdioms.forEach(function(eachIdiom) {
         Idiom.create(eachIdiom)
         .then(function(savedIdiom) {
-            // If saved successfully, print the new Idiom document to the console
+            
             console.log(savedIdiom);
         })
         .catch(function(err) {
-            // If an error occurs, log the error message
+          
             console.log(err.message);
         });
     });
